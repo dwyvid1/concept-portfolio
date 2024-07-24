9f60715bdf99c0316a067a9b928a2503f9727630
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-23 22:25:02 -0300

    Project name changes

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -3,9 +3,10 @@ import { ReactNode } from "react"
 
 interface StyledButtonProps {
     children: ReactNode
+    onclick: () => void
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
+const StyledButton: React.FC<StyledButtonProps> = ({ children, onclick }) => {
 
     const StyledButton = styled("button")(({ theme }) => ({
         backgroundColor: "transparent",
@@ -25,7 +26,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
 
     return (
         <>
-            <StyledButton>
+            <StyledButton onClick={onclick}>
                 {children}
             </StyledButton>
         </>
